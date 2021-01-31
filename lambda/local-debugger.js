79a5c62758fb0748d6caf4590926b1536c80f10f
Evann DREUMONT <53308142+LeGmask@users.noreply.github.com>
2021-01-31 17:54:01 +0100

    Format using tabs

--- lambda/local-debugger.js
+++ lambda/local-debugger.js
@@ -22,15 +22,15 @@ use another editor/IDE, please check out the [ASK SDK Local Debug package at npm
 
 */
 
-const net = require('net');
-const fs = require('fs');
+const net = require("net");
+const fs = require("fs");
 
 const localDebugger = net.createServer();
 
-const httpHeaderDelimeter = '\r\n';
-const httpBodyDelimeter = '\r\n\r\n';
-const defaultHandlerName = 'handler';
-const host = 'localhost';
+const httpHeaderDelimeter = "\r\n";
+const httpBodyDelimeter = "\r\n\r\n";
+const defaultHandlerName = "handler";
+const host = "localhost";
 const defaultPort = 0;
 
 /**
@@ -48,7 +48,7 @@ const lambdaHandlerName = getLambdaHandlerName();
  */
 
 localDebugger.listen(portNumber, host, () => {
-    console.log(`Starting server on port: ${localDebugger.address().port}.`);
+	console.log(`Starting server on port: ${localDebugger.address().port}.`);
 });
 
 /**
@@ -60,17 +60,21 @@ localDebugger.listen(portNumber, host, () => {
  * The response is written onto the socket connection.
  */
 
-localDebugger.on('connection', (socket) => {
-    console.log(`Connection from: ${socket.remoteAddress}:${socket.remotePort}`);
-    socket.on('data', (data) => {
-        const body = JSON.parse(data.toString().split(httpBodyDelimeter).pop());
-        console.log(`Request envelope: ${JSON.stringify(body)}`);
-        skillInvoker[lambdaHandlerName](body, null, (_invokeErr, response) => {
-            response = JSON.stringify(response);
-            console.log(`Response envelope: ${response}`);
-            socket.write(`HTTP/1.1 200 OK${httpHeaderDelimeter}Content-Type: application/json;charset=UTF-8${httpHeaderDelimeter}Content-Length: ${response.length}${httpBodyDelimeter}${response}`);
-        });
-    });
+localDebugger.on("connection", (socket) => {
+	console.log(
+		`Connection from: ${socket.remoteAddress}:${socket.remotePort}`
+	);
+	socket.on("data", (data) => {
+		const body = JSON.parse(data.toString().split(httpBodyDelimeter).pop());
+		console.log(`Request envelope: ${JSON.stringify(body)}`);
+		skillInvoker[lambdaHandlerName](body, null, (_invokeErr, response) => {
+			response = JSON.stringify(response);
+			console.log(`Response envelope: ${response}`);
+			socket.write(
+				`HTTP/1.1 200 OK${httpHeaderDelimeter}Content-Type: application/json;charset=UTF-8${httpHeaderDelimeter}Content-Length: ${response.length}${httpBodyDelimeter}${response}`
+			);
+		});
+	});
 });
 
 /**
@@ -79,18 +83,24 @@ localDebugger.on('connection', (socket) => {
  */
 
 function getAndValidatePortNumber() {
-    const portNumberArgument = Number(getArgument('portNumber', defaultPort));
-    if (!Number.isInteger(portNumberArgument)) {
-        throw new Error(`Port number has to be an integer - ${portNumberArgument}.`);
-    }
-    if (portNumberArgument < 0 || portNumberArgument > 65535) {
-        throw new Error(`Port out of legal range: ${portNumberArgument}. The port number should be in the range [0, 65535]`);
-    }
-    if (portNumberArgument === 0) {
-        console.log('The TCP server will listen on a port that is free.'
-        + 'Check logs to find out what port number is being used');
-    }
-    return portNumberArgument;
+	const portNumberArgument = Number(getArgument("portNumber", defaultPort));
+	if (!Number.isInteger(portNumberArgument)) {
+		throw new Error(
+			`Port number has to be an integer - ${portNumberArgument}.`
+		);
+	}
+	if (portNumberArgument < 0 || portNumberArgument > 65535) {
+		throw new Error(
+			`Port out of legal range: ${portNumberArgument}. The port number should be in the range [0, 65535]`
+		);
+	}
+	if (portNumberArgument === 0) {
+		console.log(
+			"The TCP server will listen on a port that is free." +
+				"Check logs to find out what port number is being used"
+		);
+	}
+	return portNumberArgument;
 }
 
 /**
@@ -99,7 +109,7 @@ function getAndValidatePortNumber() {
  */
 
 function getLambdaHandlerName() {
-    return getArgument('lambdaHandler', defaultHandlerName);
+	return getArgument("lambdaHandler", defaultHandlerName);
 }
 
 /**
@@ -109,11 +119,11 @@ function getLambdaHandlerName() {
 
 // eslint-disable-next-line consistent-return
 function getAndValidateSkillInvokerFile() {
-    const fileNameArgument = getArgument('skillEntryFile');
-    if (!fs.existsSync(fileNameArgument)) {
-        throw new Error(`File not found: ${fileNameArgument}`);
-    }
-    return fileNameArgument;
+	const fileNameArgument = getArgument("skillEntryFile");
+	if (!fs.existsSync(fileNameArgument)) {
+		throw new Error(`File not found: ${fileNameArgument}`);
+	}
+	return fileNameArgument;
 }
 
 /**
@@ -123,13 +133,15 @@ function getAndValidateSkillInvokerFile() {
  */
 
 function getArgument(argumentName, defaultValue) {
-    const index = process.argv.indexOf(`--${argumentName}`);
-    if (index === -1 || typeof process.argv[index + 1] === 'undefined') {
-        if (defaultValue === undefined) {
-            throw new Error(`Required argument - ${argumentName} not provided.`);
-        } else {
-            return defaultValue;
-        }
-    }
-    return process.argv[index + 1];
+	const index = process.argv.indexOf(`--${argumentName}`);
+	if (index === -1 || typeof process.argv[index + 1] === "undefined") {
+		if (defaultValue === undefined) {
+			throw new Error(
+				`Required argument - ${argumentName} not provided.`
+			);
+		} else {
+			return defaultValue;
+		}
+	}
+	return process.argv[index + 1];
 }
--- lambda/logger.js
+++ lambda/logger.js
@@ -1,10 +1,10 @@
 const Logger = require("js-logger");
 
 Logger.useDefaults({
-  defaultLevel: Logger.DEBUG,
-  formatter: function (messages, context) {
-    messages.unshift(`[${context.level.name}]`);
-  },
+	defaultLevel: Logger.DEBUG,
+	formatter: function (messages, context) {
+		messages.unshift(`[${context.level.name}]`);
+	},
 });
 
 module.exports = Logger;
