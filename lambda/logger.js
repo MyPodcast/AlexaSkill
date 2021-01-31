0c7638ecfe80bb051ef84e30f159ff63e37f1c9f
Evann DREUMONT <53308142+LeGmask@users.noreply.github.com>
2021-01-31 15:56:50 +0100

    [Logger] Add js-logger

---
+++ lambda/logger.js
@@ -0,0 +1,10 @@
+const Logger = require("js-logger");
+
+Logger.useDefaults({
+  defaultLevel: Logger.DEBUG,
+  formatter: function (messages, context) {
+    messages.unshift(`[${context.level.name}]`);
+  },
+});
+
+module.exports = Logger;
